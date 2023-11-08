3d6d9513bb61ad48bc8c9dd95e8dacfb4531f3be
Adriele Lima <adriele.p.t@hotmail.com>
2023-11-08 22:37:51 +0000

    ADD: toast templates and message levels

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.contrib import messages
 
+from products.models import Product
 
 def view_bag(request):
     """ A view to return bag page """
@@ -10,6 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of specific product to the shopping bag """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
@@ -18,6 +21,7 @@ def add_to_bag(request, item_id):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
