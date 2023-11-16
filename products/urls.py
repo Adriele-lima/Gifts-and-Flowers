dc75e2babf0843a2d91eddad75370c2e77d6d6ec
Adriele Lima <adriele.p.t@hotmail.com>
2023-11-16 22:36:38 +0000

    ADD: delete functionality and buttons for delete and edit

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:product_id>/', views.view_product, name='view_product'),
     path('add/', views.add_product, name='add_product'),
     path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),
 ]
