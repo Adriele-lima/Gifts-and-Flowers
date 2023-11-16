a53197c52c9f2a138f51cb98c244595d9a0697ca
Adriele Lima <adriele.p.t@hotmail.com>
2023-11-16 19:10:15 +0000

    ADD: add product function Creating form for add new product Creating view Creating template Adding URL Adding some style

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from .forms import ProductForm
 
 
 def all_products(request):
@@ -59,3 +60,14 @@ def view_product(request, product_id):
     }
 
     return render(request, 'products/view_product.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
