ad047a711aaf19aa1b54cb50f4f2a6fb263d348a
Adriele Lima <adriele.p.t@hotmail.com>
2023-11-16 23:55:04 +0000

    ADD/UPDATE: image select box Adding widgets file to style image box Updating style for image box

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
@@ -8,10 +9,12 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False,
+                             widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         self.fields['category'].choices = friendly_names
-
