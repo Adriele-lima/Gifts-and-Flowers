5c418406522b4ff071148df761262a0c686f5db6
Adriele Lima <adriele.p.t@hotmail.com>
2023-11-27 00:51:30 +0000

    ADD: creating a contact page app

---
+++ contact/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def contact(request):
+    """ A view to return contact page """
+
+    return render(request, 'contact/contact.html')
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+    'contact',
 
     # Other
     'crispy_forms',
